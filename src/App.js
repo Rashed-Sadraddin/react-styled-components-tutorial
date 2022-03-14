ce462d40be6fbd1260491b9b7a58858d1d1065c6
Rashed-Sadraddin <96119144+Rashed-Sadraddin@users.noreply.github.com>
2022-03-14 17:50:09 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
 import './App.css';
-import Header from './components/Header'
+import {useState,useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import { todoActions } from './store/todoSlice';
 
-import {Routes, Route} from 'react-router-dom'
-
-import Home from './pages/Home/Home'
-import Contacts from './pages/Contact/Contacts'
+let isFirst = true
 
 function App() {
+  const todos = useSelector(state => state.todo.todos);
+  const dispatch =  useDispatch()
+
+  const [inputText,setInputText] = useState('')
+
+  const onRemove = (id)=>{
+    dispatch(todoActions.removeTodo(id))
+  }
+  const onAdd = (txt)=>{
+    dispatch(todoActions.addTodo(txt))
+  }
+  const onInput = (e)=>{
+    setInputText(e.target.value)
+  }
+
+  // useEffect(() => {
+  //   fetch('https://todo-demo-e0f6b-default-rtdb.firebaseio.com/todos.json')
+  //   .then(async(res)=>{
+  //     const data = await res.json();
+  //     dispatch(todoActions.replaceTodos(data))
+  //   })
+  //   .catch(err=>{
+  //       console.log(err)}
+  //   )
+  // },[])
+
+  useEffect(()=>{
+    if(!isFirst){
+      fetch('https://todo-demo-e0f6b-default-rtdb.firebaseio.com/todos.json', {
+      method:'PUT',
+      // mode: 'cors',     
+      // headers: new Headers({       'Content-Type': 'application/json'    }),
+      body: JSON.stringify(todos)
+      }).then(res=>{console.log(res)}).catch(err=>{console.log(err)})
+    }
+    isFirst=false
+  },[todos])
+
   return (
-    <>
-    
-    <Header></Header>
+    <div className="App">
+      <input onChange={onInput}/>
+      <button onClick={()=>{onAdd(inputText)}}>Add todo</button>
 
-    <Routes>
-
-      <Route path='/' element={<Home/>} />
-      <Route path='contact/*' element={<Contacts/>} >
-        {/* <Route path=':contactOP' element={<Contact/>} /> */}
-      </Route>
-
-    </Routes>
-    
-    </>
-
+      <div>
+        {
+          todos.map(todo => {
+            return <div key={todo.id} style={{margin:'10px', background:'grey', borderRadius:'15px', display:'flex', flexDirection: 'row', alignItems:'center', justifyContent:'space-between', padding:'10px'}}>
+              <h2>{todo.text}</h2>
+              <button onClick={()=>{onRemove(todo.id)}}>delete</button>
+            </div>
+          })
+        }
+      </div>
+    </div>
   );
 }
 
--- src/pages/Home/Home.js
+++
@@ -1,8 +0,0 @@
-
-const Home = () =>{
-    return ( 
-        <h1 style={{paddingInline:'20px'}}>Home page</h1>
-    )
-}
-
-export default Home
